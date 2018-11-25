0de4710a75455c62e231dd725b2c58c9cbbe0e87
Bruno Aybar <miguel_bruno@hotmail.com>
2018-11-25 16:35:53 -0500

    Setup meetup details toolbar

--- app/src/main/java/dev/android/peru/modules/meetup/detail/MeetupDetailFragment.kt
+++ app/src/main/java/dev/android/peru/modules/meetup/detail/MeetupDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 
 import dev.android.peru.R
@@ -12,6 +13,7 @@ import dev.android.peru.provide
 import peru.android.dev.baseutils.exhaustive
 import dev.android.peru.modules.meetup.detail.MeetupUiState.*
 import kotlinx.android.synthetic.main.meetup_detail_fragment.*
+import peru.android.dev.androidutils.toast
 import peru.android.dev.datamodel.Meetup
 
 class MeetupDetailFragment : Fragment() {
@@ -32,6 +34,7 @@ class MeetupDetailFragment : Fragment() {
         viewModel = provide()
         bind()
         loadMeetup()
+        setupToolbar()
     }
 
     private fun bind() {
@@ -50,6 +53,23 @@ class MeetupDetailFragment : Fragment() {
     private fun display(meetup: Meetup) {
         meetupTitleTextView.text = meetup.name
         meetupDescriptionTextView.text = meetup.description
+    }
+
+    private fun setupToolbar() {
+        with(meetupDetailToolbar) {
+            navigationIcon = ContextCompat.getDrawable(context, R.drawable.ic_close_white_24dp)
+            inflateMenu(R.menu.menu_meetup_detail)
+            setOnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    R.id.menu_start_attendance -> {  }
+                    R.id.menu_start_questionnaire -> { }
+                }
+                true
+            }
+            setNavigationOnClickListener {
+                //todo
+            }
+        }
     }
 
     private fun loadMeetup() {
