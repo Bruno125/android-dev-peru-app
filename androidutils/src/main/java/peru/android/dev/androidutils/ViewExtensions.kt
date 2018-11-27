a93f0447b64c3ca1b403e9ddcbf3269bbba659f0
Bruno Aybar <miguel.aybar@avantica.net>
2018-11-27 10:03:13 -0500

    Add circular reveal to the questionnaire finish animation

--- androidutils/src/main/java/peru/android/dev/androidutils/ViewExtensions.kt
+++ androidutils/src/main/java/peru/android/dev/androidutils/ViewExtensions.kt
@@ -1,7 +1,31 @@
 package peru.android.dev.androidutils
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.os.Build
 import android.view.View
+import android.view.ViewAnimationUtils
+import androidx.annotation.RequiresApi
 
 fun View.updateVisibility(visible: Boolean) {
-    visibility = if(visible) View.VISIBLE else View.INVISIBLE
+    visibility = if (visible) View.VISIBLE else View.INVISIBLE
+}
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+fun View.reversedCenteredCircularReveal(duration: Long = 1000L,
+                                        onAnimationEnd: () -> Unit = {}) {
+    val centerX = width / 2
+    val centerY = height / 2
+    ViewAnimationUtils.createCircularReveal(this, centerX, centerY,
+            width.toFloat(), 0f).apply {
+        this.duration = duration
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                visibility = View.GONE
+                onAnimationEnd()
+            }
+        })
+        start()
+    }
 }
